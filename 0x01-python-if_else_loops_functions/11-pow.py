d74094cf9f702ea53ecfe80890de527211058ddb
Exaltprecious <peculiarchristj1234@gmail.com>
2024-03-12 18:22:47 -0700

    0-positive_or_negative.py

---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+# Author - Chinelo Peculiar
+def pow(a, b):
+    return (a ** b)
---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+# Authot -Eneh Chinelo Peculiar
+for i in range(97, 123):
+    if chr(i) == 'q' or chr(i) == 'e':
+        continue
+    print(chr(i).format(i), end='')
