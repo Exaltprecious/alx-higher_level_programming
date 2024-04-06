258875201a6ba20b7f4e86bd012c1fec0161a4a9
Exaltprecious <peculiarchristj1234@gmail.com>
2024-04-06 09:29:21 -0700

    2-safe_print_list_integers.py

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
-
 def safe_print_list_integers(my_list=[], x=0):
-    count = 0
+    """
+    This function prints all ints of a list, raises an Exception
+    only when x is greater than lenght of the list
+    """
+    printedLen = 0
     for i in range(x):
         try:
-            if isinstance(my_list[i], int):
-                print("{:d}".format(my_list[i]), end="")
-                count += 1
-        except (TypeError, ValueError, IndexError):
-            break
+            print("{:d}".format(my_list[i]), end="")
+            printedLen += 1
+        except (ValueError, TypeError):
+            continue
     print()
-    return counet number
+    return printedLen
