ec47c9bdfc2b7af7abaca8402fbe7f06889db405
Exaltprecious <peculiarchristj1234@gmail.com>
2024-04-23 07:39:28 -0700

    10-square.py

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,11 +1,19 @@
-#!/usr/bin/python3
-# 1-my_list.py
-"""Defining an inherited list class MyList."""
+p#!/usr/bin/python3
+# 10-square.py
+# Brennan D Baraban <375@holbertonschool.com>
+"""Defines a Rectangle subclass Square."""
+Rectangle = __import__('9-rectangle').Rectangle
 
 
-class MyList(list):
-    """Implementing sorted printing for the built-in list class."""
+class Square(Rectangle):
+        """Represent a square."""
 
-    def print_sorted(self):
-        """Printing a list in sorted ascending order."""
-        print(sorted(self))
+    def __init__(self, size):
+        """Initialize a new square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
