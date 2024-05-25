507b2f78f8ae96ec696e20796d9612c29e54bbfe
Exaltprecious <peculiarchristj1234@gmail.com>
2024-05-25 10:12:56 -0700

    0x0D-SQL_introduction

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,2 @@
+-- prints the full description of the table first_table 4rm d database hbtn_0c_0 in my MySQL server.
+-- SHOW CREATE DATABASE databaseName -> Show the CREATE DATABASE statement
---
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -0,0 +1,4 @@
+-- updates the score of Bob to 10 in the table named second_table.
+UPDATE second_table
+-- SELECT * FROM tableName -> List all the rows of ALL columns, * is a wildcard denoting all columns
+SELECT * FROM first_table
