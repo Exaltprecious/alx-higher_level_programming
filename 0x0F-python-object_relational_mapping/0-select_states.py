681698ddd31f53bb45446a39f1283059870aa97e
Exaltprecious <peculiarchristj1234@gmail.com>
2024-06-23 13:05:53 -0700

    0x0F-python-object_relational_mapping

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Script that lists all states from the database hbtn_0e_0_usa
+"""
+
+import MySQLdb
+import sys
+
+def select_states():
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    # Connect to the database
+    db = MySQLdb.connect(host="localhost", port=3306, user=username,
+                         passwd=password, db=database)
+
+    # Create a cursor object
+    cur = db.cursor()
+
+    # Execute the query with LIMIT 5 and ORDER BY states.id
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC LIMIT 5")
+
+    # Connect to the database
+    db = MySQLdb.connect(host="localhost", port=3306, user=username,
+                         passwd=password, db=database)
+
+    # Create a cursor object
+    cur = db.cursor()
+
+    # Execute the query with LIMIT 5 and ORDER BY states.id
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC LIMIT 5")
---
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.sql
@@ -0,0 +1,3 @@
+Enter password: 
+ERROR 1146 (42S02) at line 5: Table 'hbtn_0e_100_usa.states' doesn't exist
+guillaume@ubuntu:~/0x0F$ ./100-relationship_states_cities.py root root hbtn_0e_100_usa
