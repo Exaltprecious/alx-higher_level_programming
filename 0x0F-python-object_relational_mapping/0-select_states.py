f5002763cbf36fc5f9172404b09349ca2c8b1772
Exaltprecious <peculiarchristj1234@gmail.com>
2024-06-23 20:04:15 -0700

    A script that lists all states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,32 +1,16 @@
 #!/usr/bin/python3
-"""
-Script that lists all states from the database hbtn_0e_0_usa
-"""
-
+"""  lists all states from the database hbtn_0e_0_usa """
 import MySQLdb
 import sys
 
-def select_states():
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
 
-    # Connect to the database
-    db = MySQLdb.connect(host="localhost", port=3306, user=username,
-                         passwd=password, db=database)
-
-    # Create a cursor object
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                                     passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = db.cursor()
-
-    # Execute the query with LIMIT 5 and ORDER BY states.id
-    cur.execute("SELECT * FROM states ORDER BY states.id ASC LIMIT 5")
-
-    # Connect to the database
-    db = MySQLdb.connect(host="localhost", port=3306, user=username,
-                         passwd=password, db=database)
-
-    # Create a cursor object
-    cur = db.cursor()
-
-    # Execute the query with LIMIT 5 and ORDER BY states.id
-    cur.execute("SELECT * FROM states ORDER BY states.id ASC LIMIT 5")
+    cur.execute("SELECT * FROM states")
+    rows = cur.fetchall()
+    for row in rows:
+                print(row)
+    cur.close()
+    db.close()
