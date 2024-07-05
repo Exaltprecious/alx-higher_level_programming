51e2ec1e19830d703c8f2c3c92b811f1a0f5896b
Exaltprecious <peculiarchristj1234@gmail.com>
2024-07-05 16:15:49 -0700

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
-"""Fetches https://intranet.hbtn.io/status."""
+'''Script that fetches my https://alx-intranet.htbn.io/status'''
+
 import urllib.request
 
+if __name__ == '__main__':
+    text = '''Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 content: {}'''
+    url = 'https://alx-intranet.hbtn.io/status'
 
-if __name__ == "__main__":
-    request = urllib.request.Request("https://intranet.hbtn.io/status")
-        with urllib.request.urlopen(request) as response:
-        body = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(body)))
-                print("\t- content: {}".format(body))
-        print("\t- utf8 content: {}".format(body.decode("utf-8")))
+with urllib.request.urlopen(url) as response:
+    body = response.read()
+
+    print("Body response:")
+print("\t- type:", type(body))
+print("\t- content:", body)
+print("\t- utf8 content:", body.decode('utf-8'))
