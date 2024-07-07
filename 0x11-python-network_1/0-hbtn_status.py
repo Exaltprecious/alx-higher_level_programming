60003addaa795099b34fae2ae7f1a38db0b20be3
Exaltprecious <peculiarchristj1234@gmail.com>
2024-07-07 01:57:14 -0700

    Python script that fetches

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-'''Script that fetches my https://alx-intranet.htbn.io/status'''
-
+"""Fetches https://intranet.hbtn.io/status"""
 import urllib.request
 
-if __name__ == '__main__':
-    text = '''Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 content: {}'''
-    url = 'https://alx-intranet.hbtn.io/status'
 
-with urllib.request.urlopen(url) as response:
-    body = response.read()
+def fetcher():
+        """fetcher"""
+    with urllib.request.urlopen("https://intranet.hbtn.io/status") as response:
+        html = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(html)))
+                print("\t- content: {}".format(html))
+        print("\t- utf8 content: {}".format(html.decode("utf-8")))
 
-    print("Body response:")
-print("\t- type:", type(body))
-print("\t- content:", body)
-print("\t- utf8 content:", body.decode('utf-8'))
+if __name__ == "__main__":
+    fetcher()
