608604b76822e7057471cc31805cb0d278c18db2
Exaltprecious <peculiarchristj1234@gmail.com>
2024-07-07 17:29:31 -0700

    4-header.sh

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-It takes in a URL as an argument, sends a GET request to the URL,
-curl -sH "X-School-User-Id: 98" "$1"r
+# script that takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response
+curl -s "$1" -H "X-School-User-Id: 98'
