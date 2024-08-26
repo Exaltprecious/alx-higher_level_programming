9209d897168d8653149e378ad15a9ebb469f5a38
Exaltprecious <peculiarchristj1234@gmail.com>
2024-08-26 09:30:35 -0700

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
-"""Fetches https://intranet.hbtn.io/status"""
+"""This script fetches data from
+    https://alx-intranet.hbtn.io/status
+"""
+
+
 import urllib.request
 
-
-def fetcher():
-        """fetcher"""
-    with urllib.request.urlopen("https://intranet.hbtn.io/status") as response:
-        html = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(html)))
-                print("\t- content: {}".format(html))
-        print("\t- utf8 content: {}".format(html.decode("utf-8")))
-
-if __name__ == "__main__":
-    fetcher()
+with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as response:
+        body = response.read()
+    print("Body response:")
+    print("\t- type: {}".format(type(body)))
+    print("\t- content: {}".format(body))
+    print("\t- utf8 content: {}".format(body.decode('utf-8')))
